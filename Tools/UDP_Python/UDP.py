5199836fa6c30644dc05f7406cd36262316ed187
otiscom <matlipi65@gmail.com>
2026-06-05 18:05:25 +0200

    Reorganize repository into Unity Simulink tools and firmware folders

---
+++ Tools/UDP_Python/UDP.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print(f"Serwer UDP aktywny. Slucham na porcie {UDP_PORT}...")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(f"Dane od {addr}: {data.decode(errors='ignore')}")
